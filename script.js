b4d67adfd3b93dad956e92213cbdd55803412626
Alban Rrahmani <albanrr9@gmail.com>
2023-11-14 15:46:09 +0100

    1

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 function menu() {
-  var x = document.getElementById("searchbar");
+  var x = document.getElementById("navbar-elements");
   if (x.style.display === "none") {
     x.style.display = "flex";
   } else {
     x.style.display = "none";
-  }
-  var y = document.getElementById("buttons");
-  if (y.style.display === "none") {
-    y.style.display = "flex";
-  } else {
-    y.style.display = "none";
   }
 }
