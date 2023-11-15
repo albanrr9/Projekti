111519943acae91eb14ead194b738d24c32a84a2
Alban Rrahmani <albanrr9@gmail.com>
2023-11-15 18:41:16 +0100

    1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function menu() {
+  var x = document.getElementById("navbar-elements");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
--- script.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-  var x = document.getElementById("navbar-elements");
-  if (x.style.display === "none") {
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none";
-  }
-}
