d4eee4fea88ce7d806cce99b1c632436b1e39426
albanrr9-k2-dell <78912350+albanrr9@users.noreply.github.com>
2023-11-27 12:41:09 +0100

    1

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ function menu() {
     x.style.display = "none";
   }
 }
+
+// Get the current year and update the copyright year
+document.getElementById('copyright-year').textContent = new Date().getFullYear();
+
+// Back to top button functionality
+const backToTopBtn = document.getElementById('back-to-top-btn');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = 'block';
+    } else {
+        backToTopBtn.style.display = 'none';
+    }
+});
+
+backToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
