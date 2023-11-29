798cf5017403c3f1538856cf83231b87981b59b3
Alban Rrahmani <albanrr9@gmail.com>
2023-11-29 23:09:26 +0100

    footer designing

--- js/script.js
+++ js/script.js
@@ -7,21 +7,3 @@ function menu() {
   }
 }
 
-// Get the current year and update the copyright year
-document.getElementById('copyright-year').textContent = new Date().getFullYear();
-
-// Back to top button functionality
-const backToTopBtn = document.getElementById('back-to-top-btn');
-
-window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopBtn.style.display = 'block';
-    } else {
-        backToTopBtn.style.display = 'none';
-    }
-});
-
-backToTopBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-});
