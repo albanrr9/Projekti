5cc66e43191faa57fd293e8b58cf0e8200d545b2
albanrr9-hp2-biblioteka2 <78912350+albanrr9@users.noreply.github.com>
2023-12-29 10:01:49 +0100

    added search function

---
+++ js/search.js
@@ -0,0 +1,28 @@
+function search() {
+  const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+  const queryParams = `?search=${encodeURIComponent(searchTerm)}`;
+  window.location.href = `pages/search-results.html${queryParams}`;
+}
+
+function handleKeyPress(event) {
+  // Check if the pressed key is Enter (key code 13)
+  if (event.key === 'Enter') {
+    // Trigger the search function
+    event.preventDefault();
+    return search();
+  }
+}
+function search1() {
+  const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+  const queryParams = `?search=${encodeURIComponent(searchTerm)}`;
+  window.location.href = `/pages/search-results.html${queryParams}`;
+}
+
+function handleKeyPress1(event) {
+  // Check if the pressed key is Enter (key code 13)
+  if (event.key === 'Enter') {
+    // Trigger the search function
+    event.preventDefault();
+    return search1();
+  }
+}
